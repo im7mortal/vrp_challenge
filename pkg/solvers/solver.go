152e9cd3465b0fdda8c7c2b9b41dd70237831e3b
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-06-21 22:42:48 -0400

    jumping around with solutions

--- pkg/solvers/solver.go
+++ pkg/solvers/solver.go
@@ -1,9 +1,43 @@
 package solvers
 
-func Solve1(vals [][4]float64) (sol [][]int) {
+import (
+	"math"
+)
 
-	for i := range vals {
-		sol = append(sol, []int{i + 1})
+//// we know that negative mustn't be bigger then 12 hours shift
+//const MaxFor12HoursShift float64 = 1000
+//
+//func Normalize(coord *[][4]float64, offset float64) {
+//	for i := range *coord {
+//		(*coord)[i][0] += offset
+//		(*coord)[i][1] += offset
+//		(*coord)[i][2] += offset
+//		(*coord)[i][3] += offset
+//	}
+//}
+
+func euclidDistance(coord [4]float64) float64 {
+	return math.Sqrt(math.Pow(coord[2]-coord[0], 2) + math.Pow(coord[3]-coord[1], 2))
+}
+
+type Solver interface {
+	Solve() [][]int
+}
+
+type nearestNeighbor struct {
+	vals   [][4]float64
+	center [2]float64
+}
+
+func NewNearestNeighbor(vals [][4]float64, center [2]float64) Solver {
+	return &nearestNeighbor{vals, center}
+}
+
+func (nn *nearestNeighbor) Solve() (solution [][]int) {
+
+	for i := range nn.vals {
+		solution = append(solution, []int{i + 1})
 	}
-	return sol
+
+	return solution
 }
